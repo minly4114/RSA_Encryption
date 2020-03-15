b7b93f7106354a4c95d5e5500da34c3d0d6f9bb0
Elizaveta <lizasemenova19980@gmail.com>
2020-03-15 20:29:02 +0300

    - изменение получения e и d

--- RSA_Encryption/RSA_Encryption/Program.cs
+++ RSA_Encryption/RSA_Encryption/Program.cs
@@ -13,27 +13,53 @@ namespace RSA_Encryption
             var p = GetPrimeNumber();
             var q = GetPrimeNumber();
             uint n = p * q;
-            uint d = GetPrimeNumber();
-            //uint e = GetPrimeNumber();
-            uint e = GetOpen(d, p,q);
-            var text = Console.ReadLine().ToLower().ToCharArray().ToList();
-            var list = text.ConvertAll(x => Convert.ToInt32( x)-1072);
-            var cryptList = list.ConvertAll(x => Crypt((uint)x, e, n));
-            var resultCrypt = cryptList.ConvertAll(x => Convert.ToChar(x));
-            string s = "";
-            resultCrypt.ForEach(x => s += x);
-            Console.WriteLine(s);
-            var resultDeCrypt = resultCrypt.ConvertAll(x => Convert.ToInt32(x) - 1072).ConvertAll(x => Decrypt((uint)x, d, n)).ConvertAll(x => Convert.ToChar(x));
-            var s2 = "";
-            resultDeCrypt.ForEach(x => s2 += x);
-            Console.WriteLine(s2);
-            Console.ReadLine();
+            ulong fi = (p - 1) * (q - 1);
+            ulong e = GetOpen(fi);
+            ulong d = GetPrivate(e,fi);
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие/n/r1-Зашифровать/n/r2-Расшивровать/n/rДругой символ -Выход");
+                var com = Console.ReadLine();
+                if (int.TryParse(com, out int command))
+                {
+                    if (command == 1)
+                    {
+                        var list = Console.ReadLine().ToLower().ToCharArray().ToList();
+                        var cryptList = list.ConvertAll(x => Crypt((uint)x, e, n));
+                        string s = "";
+                        cryptList.ForEach(x => s += x + "-");
+                        s.Remove(s.Length - 1);
+                        Console.WriteLine(s);
+                    }
+                    else if (command == 2)
+                    {
+                        var text = Console.ReadLine();
+                        Regex regex = new Regex("\\d+-{1}");
+
+                        var match = regex.Matches(text).ToList();
+                        var list = match.ConvertAll(x => Convert.ToUInt64(x.ToString().Replace("-", "")));
+                        var resultDeCrypt = list.ConvertAll(x => Decrypt(x, d, n)).ConvertAll(x => Convert.ToChar(x));
+                        var s2 = "";
+                        resultDeCrypt.ForEach(x => s2 += x);
+                        Console.WriteLine(s2);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
 
         private static uint GetPrimeNumber()
         {
             Random random = new Random();
-            using(FileStream fs = new FileStream(Environment.CurrentDirectory+"\\ListPrimeNumber.txt",FileMode.Open))
+            using (FileStream fs = new FileStream(Environment.CurrentDirectory + "\\ListPrimeNumber.txt", FileMode.Open))
             {
                 byte[] array = new byte[fs.Length];
                 fs.Read(array, 0, array.Length);
@@ -44,29 +70,54 @@ namespace RSA_Encryption
                 return Convert.ToUInt32(mat[rand].Value);
             }
         }
-        private static uint GetOpen(uint d, uint p, uint q)
+        private static ulong GetOpen(ulong fi)
         {
-            ulong fi = (p - 1) * (q - 1);
-            ulong e=0;
-            for (ulong i = 0; i < 18446744073709551614; i++)
+            for (ulong e = 2; e < fi; e++)
             {
-                var a = (fi*i + 1) %d;
-                ulong b0 = 0;
-                if (a == b0) {
-                    e = (fi * i + 1) / d;
-                    break;
+                if (GreatestCommonDivisor(e, fi) == 1)
+                {
+                    return e;
                 }
             }
-            return (uint)e;
+            return 0;
         }
-        private static int Crypt(uint main, uint e, uint n)
+
+        private static ulong GreatestCommonDivisor(ulong e, ulong fi)
         {
-            var mod = Convert.ToInt32(Convert.ToString( BigInteger.ModPow(main, (ulong)e, (ulong)n)))+1072;
+            while (e != 0 && fi != 0)
+            {
+                if (e > fi)
+                    e %= fi;
+                else
+                    fi %= e;
+            }
+
+            return e == 0 ? fi : e;
+        }
+
+        private static ulong GetPrivate(ulong e, ulong fi)
+        {
+            ulong d;
+            ulong temp = 1;
+
+            while(true)
+            {
+                temp += fi;
+                if(temp%e==0)
+                {
+                    d = (temp / e);
+                    return d;
+                }
+            }
+        }
+        private static int Crypt(ulong main, ulong e, ulong n)
+        {
+            var mod = Convert.ToInt32(Convert.ToString( BigInteger.ModPow(main, e, n)));
             return mod;
         }
-        private static int Decrypt(uint main, uint d, uint n)
+        private static int Decrypt(ulong main, ulong d, ulong n)
         {
-            var mod = Convert.ToInt32(Convert.ToString(BigInteger.ModPow(main, (ulong)d, (ulong)n))) + 1072;
+            var mod = Convert.ToInt32(Convert.ToString(BigInteger.ModPow(main, d, n)));
             return mod;
         }
     }
